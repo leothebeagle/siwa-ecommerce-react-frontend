b46b9152367154c721c451e65804cce210691e22
leothebeagle <a.kosber@gmail.com>
2020-10-16 17:04:27 -0400

    Write handleChange function for the form to use

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -6,13 +6,19 @@ class SignUp extends React.Component {
         firstName: "Aziz",
         lastName: "Kosber"
     }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
     
     render() {
         return(
             <div>
                 <h4>Signup form</h4>
                 <form>
-                    <input type="text" name="firstName" value={this.state.firstName} />
+                    <input type="text" name="firstName" value={this.state.firstName} onChange={event => this.handleChange(event)} />
                     <input type="text" name="lastName" value={this.state.lastName} />
                 </form>
             </div>
