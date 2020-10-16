abb1a0937f929780510753967ad665753a0ed976
leothebeagle <a.kosber@gmail.com>
2020-10-16 16:47:34 -0400

    Setup form to derive input values from local state

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 class SignUp extends React.Component {
+    
+    state = {
+        firstName: "Aziz",
+        lastName: "Kosber"
+    }
+    
     render() {
         return(
-            <h4>Signup form</h4>
+            <div>
+                <h4>Signup form</h4>
+                <form>
+                    <input type="text" name="firstName" value={this.state.firstName} />
+                    <input type="text" name="lastName" value={this.state.lastName} />
+                </form>
+            </div>
         )
     }
 }
