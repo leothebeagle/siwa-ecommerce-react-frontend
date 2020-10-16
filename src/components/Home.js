84b2c45724cb31ffc4ef707ca8d3365b9b000dea
leothebeagle <a.kosber@gmail.com>
2020-10-16 16:46:33 -0400

    Render <SignUp /> in <Home />

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import SignUp from './SignUp';
 
 class Home extends Component {
     render() {
         return(
-            <h1>Homepage</h1>
+            <div>
+                <h1>Homepage</h1>
+                <SignUp />
+            </div>
         )
     }
 }
