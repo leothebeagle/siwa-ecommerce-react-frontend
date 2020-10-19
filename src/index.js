4acde6678f8a64f3c444e39a46e8d6eab7dad396
leothebeagle <a.kosber@gmail.com>
2020-10-19 17:55:45 -0400

    Remove importing Router into index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
