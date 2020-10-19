4e11ccd1a62328abf832cde6b08f8ab2b613c0dc
leothebeagle <a.kosber@gmail.com>
2020-10-18 20:08:18 -0400

    Inclued Navabar in Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Navbar from '../containers/Navbar';
 
 class Home extends Component {
     render() {
         return(
             <div>
                 <h1>Homepage</h1>
+                <Navbar />
             </div>
         )
     }
