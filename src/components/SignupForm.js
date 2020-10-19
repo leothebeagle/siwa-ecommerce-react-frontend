727d3c2e577d0c2386db863009124a8edd22427c
leothebeagle <a.kosber@gmail.com>
2020-10-19 01:17:13 -0400

    Cleanup SignupForm

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 function SignupForm(props) {
 
-        const {
-            handleChange,
-            handleSubmit
-        } = props
+    const {
+        handleChange,
+        handleSubmit
+    } = props
         
-      return(
-            <div>
-                <h4>Signup form</h4>
-                <form onSubmit={handleSubmit} >
-                    <input onChange={handleChange} type="text" name="firstName" value={props.firstName} placeholder="First Name" />
-                    <input onChange={handleChange} type="text" name="lastName" value={props.lastName} placeholder="Last Name"/>
-                    <input onChange={handleChange} type="email" name="email" value={props.email} placeholder="email"/>
-                    <input onChange={handleChange} type="password" name="password" value={props.password} placeholder="password"/>
-                    <input onChange={handleChange} type="password" name="passwordConfirmation" value={props.password_confirmation} placeholder="password confirmation"/>
-                    <button type="submit">Register</button>
-                </form>
-            </div>
-      )
+    return(
+        <div>
+            <h4>Signup form</h4>
+            <form onSubmit={handleSubmit} >
+                <input onChange={handleChange} type="text" name="firstName" value={props.firstName} placeholder="First Name" />
+                <input onChange={handleChange} type="text" name="lastName" value={props.lastName} placeholder="Last Name"/>
+                <input onChange={handleChange} type="email" name="email" value={props.email} placeholder="email"/>
+                <input onChange={handleChange} type="password" name="password" value={props.password} placeholder="password"/>
+                <input onChange={handleChange} type="password" name="passwordConfirmation" value={props.password_confirmation} placeholder="password confirmation"/>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    )
 }
 export default SignupForm;
