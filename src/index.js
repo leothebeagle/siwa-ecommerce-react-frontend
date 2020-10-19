3bc8f08594d3f267fe46bbb42382e8bc1b2137da
leothebeagle <a.kosber@gmail.com>
2020-10-19 17:29:14 -0400

    Remove wrapping <Router> around <App/> in index.js

--- src/index.js
+++ src/index.js
@@ -17,9 +17,7 @@ const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
-      <Router>
         <App />
-      </Router>
     </Provider>,
   document.getElementById('root')
 );
