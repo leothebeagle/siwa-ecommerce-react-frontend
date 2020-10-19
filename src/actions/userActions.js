2c089843a51e0f48b9f1791d887cd464adc28f79
leothebeagle <a.kosber@gmail.com>
2020-10-19 17:36:25 -0400

    Remove Router from index.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,7 +1,22 @@
-const addUser = (user) => {
-    return {
-        type: 'ADD_USER',
-        payload: user
+// const addUser = (user) => {
+//     return {
+//         type: 'ADD_USER',
+//         payload: user
+//     }
+// }
+
+function addUser(user) {
+    return (dispatch) => {
+        fetch('http://localhost:3000/users', 
+        {
+          method: 'POST',  
+          mode: 'no-cors',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(user)
+        }) 
+        .then(resp => console.log(resp))
     }
 }
 
