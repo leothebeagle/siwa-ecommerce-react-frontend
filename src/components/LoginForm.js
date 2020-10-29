a8021d616c52f9316a71ebc834cda9ea58c692d1
leothebeagle <a.kosber@gmail.com>
2020-10-29 18:22:46 -0400

    Correct title for login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,7 +9,7 @@ const LoginForm = (props) => {
         
     return(
         <div>
-            <h4>Signup form</h4>
+            <h4>Login form</h4>
             <form onSubmit={handleSubmit} >
                 <input onChange={handleChange} type="email" name="email" value={props.email} placeholder="email"/>
                 <input onChange={handleChange} type="password" name="password" value={props.password} placeholder="password"/>
