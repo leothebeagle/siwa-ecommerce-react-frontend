f75054c0e9eb501f34e02145864c8e4d3de7753f
leothebeagle <a.kosber@gmail.com>
2020-10-30 17:19:25 -0400

    Remove UserDashboard component from App

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import React from 'react';
 import Home from './containers/Home'
 import Navbar from './components/Navbar'
 import SignupPage from './containers/SignupPage';
-import UserDashboard from './containers/UserDashboard'
 import LoginPage from './containers/LoginPage'
 
 
