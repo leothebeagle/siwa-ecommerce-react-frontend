c6702fd5fa236b719a64ccabe3f0fb91b9e4f007
leothebeagle <a.kosber@gmail.com>
2020-11-02 16:02:37 -0500

    Include itemsReducer in apps main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import usersReducer from './usersReducer';
+import itemsReducer from './itemsReducer'
 
 export default combineReducers({
-    user: usersReducer
+    user: usersReducer,
+    items: itemsReducer
 })
