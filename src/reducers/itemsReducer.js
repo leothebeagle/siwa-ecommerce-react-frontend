ce7fdd9a8b3a140841c25466e6448099a1fc26b2
leothebeagle <a.kosber@gmail.com>
2020-11-03 18:17:54 -0500

    Correct fetchingUsers to fetchingItems

--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -1,20 +1,21 @@
 export default function itemsReducer(state ={
     items: [],
-    fetchingUsers: false
+    fetchingItems: false
 }, action) {
     switch(action.type) {
         case 'FETCHING_ITEMS':
             console.log("fetching users")
             return {
                 ...state,
-                fetchingUsers: true
+                fetchingItems: true
             }
 
         case 'ADD_ITEMS':
             console.log("adding items to state")
             return {
                 ...state,
-                items: action.payload
+                items: action.payload,
+                fetchingItems: false
             }
         default:
             return state
