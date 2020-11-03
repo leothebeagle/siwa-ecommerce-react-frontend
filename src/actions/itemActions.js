9b42f9eaeb695379d19645d6535ee8fc838c5c5c
leothebeagle <a.kosber@gmail.com>
2020-11-03 11:32:21 -0500

    Send FETCH request to GET items

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,5 +1,12 @@
 export const fetchItems = () => {
     return (dispatch) => {
-        dispatch({type: 'FETCHING_ITEMS'}) 
+        dispatch({type: 'FETCHING_ITEMS'})
+        
+        fetch('http://localhost:3000/items') 
+        .then(resp => resp.json())
+        .then(resp => {
+            console.log(resp)
+            //dispatch(addItems())
+        })
     }
 }
