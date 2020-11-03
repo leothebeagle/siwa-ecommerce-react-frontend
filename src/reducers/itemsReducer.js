8d6ea81bf77d550bf38c1082b8f881e03d746ca6
leothebeagle <a.kosber@gmail.com>
2020-11-03 10:36:49 -0500

    Include status of fetching users

--- src/reducers/itemsReducer.js
+++ src/reducers/itemsReducer.js
@@ -1,10 +1,20 @@
 export default function itemsReducer(state ={
-    items: []
+    items: [],
+    fetchingUsers: false
 }, action) {
     switch(action.type) {
         case 'FETCHING_ITEMS':
             console.log("fetching users")
+            return {
+                ...state,
+                fetchingUsers: true
+            }
 
+        case 'ADD_ITEMS':
+            return {
+                ...state,
+                items: action.payload
+            }
         default:
             return state
     }
