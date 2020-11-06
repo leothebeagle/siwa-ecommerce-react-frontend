277523b1a91d20f10379eb39a962613d3762530e
leothebeagle <a.kosber@gmail.com>
2020-11-05 22:30:50 -0500

    Dispatch action upon clicking 'Add to cart'

--- src/containers/ItemsList.js
+++ src/containers/ItemsList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import Item from '../components/Item'
+import Item from '../components/Item';
+import {addToCart} from '../actions/cartActions'
 
-const ItemsList = props => {
+const ItemsList = (props) => {
 
     const handleAddToCart = (item) => {
-        console.log(`Adding ${item.name} to cart`)
+        // console.log(`Adding ${item.name} to cart`)
+       props.addToCart() 
     }
 
     const items = props.items.map(item => <Item item={item} addToCart={() => handleAddToCart(item)}/>)
@@ -17,8 +19,13 @@ const ItemsList = props => {
     )
 }
 
+const mapDispatchToProps = dispatch => ({
+    addToCart: () => dispatch(addToCart())
+})
+
 const mapStateToProps = state => ({
      items: state.items.itemList
 })
 
-export default connect(mapStateToProps)(ItemsList);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemsList);
