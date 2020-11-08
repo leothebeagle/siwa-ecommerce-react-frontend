3215a9db5d89982a69b2fb8171fa4e7f396e9232
leothebeagle <a.kosber@gmail.com>
2020-11-07 20:17:26 -0500

    Write a case in CartReducer to add new cart

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -4,6 +4,11 @@ export default function cartReducer(state = {
     addingItemToCart: false
 }, action) {
     switch(action.type) {
+        case 'ADD_NEW_CART':
+            return {
+                ...state,
+                cart_id: action.payload.id
+            }
         case 'ADDING_TO_CART':
             console.log('Adding items to cart')
             return {
