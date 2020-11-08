77cd2658a4fc06aacadca5676acfcfe1a95cf76a
leothebeagle <a.kosber@gmail.com>
2020-11-08 18:42:47 -0500

    Dispatch action with item and CartId

--- src/containers/ItemsList.js
+++ src/containers/ItemsList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Item from '../components/Item';
-import {addToCart} from '../actions/cartActions'
+import {addItemToCart} from '../actions/cartActions'
 
 const ItemsList = (props) => {
 
-    const handleAddToCart = (item) => {
+    const handleAddToCartClick = (item) => {
         // console.log(`Adding ${item.name} to cart`)
-       props.addToCart() 
+       props.addItemToCart(item, props.cartId) 
     }
 
-    const items = props.items.map(item => <Item item={item} addToCart={() => handleAddToCart(item)}/>)
+    const items = props.items.map(item => <Item item={item} addItemToCart={() => handleAddToCartClick(item)}/>)
     
     return(
         <div>
@@ -20,12 +20,13 @@ const ItemsList = (props) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    addToCart: () => dispatch(addToCart())
-})
+    addItemToCart: (item, cartId) => dispatch(addItemToCart(item, cartId))
+});
 
 const mapStateToProps = state => ({
-     items: state.items.itemList
-})
+     items: state.items.itemList,
+     cartId: state.cart.cartId
+});
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(ItemsList);
