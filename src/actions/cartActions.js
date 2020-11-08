4e93fe43731128878922b578511a830e499ede26
leothebeagle <a.kosber@gmail.com>
2020-11-07 20:17:11 -0500

    Write action to add a new cart

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -1,8 +1,15 @@
 
-
+export const addNewCart = (cart) => {
+    return {
+        type: 'ADD_NEW_CART',
+        payload: cart
+    }
+}
 
 export const addToCart = (dispatch) => {
     return (dispatch) => {
         dispatch({type:'ADDING_TO_CART'})
+        //fetch request
+        // fetch response and dispatch of actions.
     }
 }
