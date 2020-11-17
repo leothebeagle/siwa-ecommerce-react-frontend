1bdf47c13f71f021739b5e9f1e2d459f48561a82
leothebeagle <a.kosber@gmail.com>
2020-11-17 18:55:13 -0500

    Set button text and correctly call Item

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Item from './Item'
 
 const Cart = (props) => {
-  
-    const cartItems = props.cartItems.map((cartItem) => <Item item={cartItem} />)
+    
+    const buttonText = "Remove Item"
+    const cartItems = props.cartItems.map((cartItem) => <Item item={cartItem} handleButtonClick={() => props.handleButtonClick(cartItem)} buttonText={buttonText} />)
     // you will pass Item the callback fn to handle deleting an item. handleButtonClick = props.handleDeleteItem(cartItem)
     // over here you should also pass in button text 
     
