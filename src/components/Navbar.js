f4a57766de85642611a1c83cfc4fa24ef061313d
leothebeagle <a.kosber@gmail.com>
2020-11-23 14:33:24 -0500

    Wrap links in nav tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,19 @@ import Logout from './Logout'
 const Navbar = props => {  
     return (
         <div>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/items'>Shop</NavLink>
+            <nav>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/items'>Shop</NavLink>
 
-            { props.user.loggedIn ?
-                <Logout handleLogoutClick={props.handleLogoutClick} /> :
-                <>
-                    <NavLink to='/login'> Login</NavLink>
-                    <NavLink to='/signup'> Signup</NavLink>
-                </>
-            }
-            <NavLink to='/cart'>Cart</NavLink>
+                { props.user.loggedIn ?
+                    <Logout handleLogoutClick={props.handleLogoutClick} /> :
+                    <>
+                        <NavLink to='/login'> Login</NavLink>
+                        <NavLink to='/signup'> Signup</NavLink>
+                    </>
+                }
+                <NavLink to='/cart'>Cart</NavLink>
+            </nav>
         </div>
     )
 }
