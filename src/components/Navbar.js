2be2befd2cea8a95253fb3bfdf635821db686c4c
leothebeagle <a.kosber@gmail.com>
2020-11-23 20:31:26 -0500

    Add className to Navlinks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,14 @@ const Navbar = props => {
             <h3>SIWA</h3>
             <nav className="navbar">
                 <ul>
-                    <li><NavLink to='/'>Home</NavLink></li>
-                    <li><NavLink to='/items'>Shop</NavLink></li>
+                    <li><NavLink to='/' className="navbar-link">Home</NavLink></li>
+                    <li><NavLink to='/items' className="navbar-link">Shop</NavLink></li>
 
                     { props.user.loggedIn ?
                         <li><Logout handleLogoutClick={props.handleLogoutClick} /> </li> :
                         <>
-                            <li> <NavLink to='/login'> Login</NavLink> </li>
-                            <li> <NavLink to='/signup'> Signup</NavLink> </li>
+                            <li> <NavLink to='/login' className="navbar-link"> Login</NavLink> </li>
+                            <li> <NavLink to='/signup' className="navbar-link"> Signup</NavLink> </li>
                         </>
                     }
                     <li> <NavLink to='/cart'>Cart</NavLink> </li>
