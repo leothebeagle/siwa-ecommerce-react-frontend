ab83f0a09aae489eb8c9906c207ad07ac8b3327b
leothebeagle <a.kosber@gmail.com>
2020-11-23 20:40:59 -0500

    Change class names

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,16 @@ const Navbar = props => {
     return (
         <header>
             <h3>SIWA</h3>
-            <nav className="navbar">
-                <ul>
-                    <li><NavLink to='/' className="navbar-link">Home</NavLink></li>
-                    <li><NavLink to='/items' className="navbar-link">Shop</NavLink></li>
+            <nav>
+                <ul className="nav-links">
+                    <li><NavLink to='/'>Home</NavLink></li>
+                    <li><NavLink to='/items'>Shop</NavLink></li>
 
                     { props.user.loggedIn ?
                         <li><Logout handleLogoutClick={props.handleLogoutClick} /> </li> :
                         <>
-                            <li> <NavLink to='/login' className="navbar-link"> Login</NavLink> </li>
-                            <li> <NavLink to='/signup' className="navbar-link"> Signup</NavLink> </li>
+                            <li> <NavLink to='/login'> Login</NavLink> </li>
+                            <li> <NavLink to='/signup'> Signup</NavLink> </li>
                         </>
                     }
                     <li> <NavLink to='/cart'>Cart</NavLink> </li>
