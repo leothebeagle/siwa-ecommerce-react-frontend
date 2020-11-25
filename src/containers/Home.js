24c705dcac5b64acfbb2d90bb1a9e6b098090c40
leothebeagle <a.kosber@gmail.com>
2020-11-25 08:58:52 -0500

    Correct they way Hero is being imported

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Hero from '../components/Hero';
 
 
 
 class Home extends Component {
+    
     render() {
         return(
             <div>
-                Home Page
+               Home Page
+               <Hero />
             </div>
         )
     }
