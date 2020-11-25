1243fef81384dd0490e3381c2e0f8ab0bc04f0b1
leothebeagle <a.kosber@gmail.com>
2020-11-24 21:32:35 -0500

    Remove comments

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 const Hero = props => {
-    return(
-        <div>
-            Hero Image Goes Here
-            {/* // dynamically populate the button text */}
-             {/* change onClick fn name in props to be generic */}
-             {/* onClick={props.handleButtonClick} */}
-        </div>
-    )
-}
+        return(
+            <div>
+                Hero Image Goes Here
+            </div>
+        )
+};
 
 
 export default Hero;
