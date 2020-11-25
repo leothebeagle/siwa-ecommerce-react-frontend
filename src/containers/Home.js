add24a5001719000303df44b0049b70ddf0d8f8c
leothebeagle <a.kosber@gmail.com>
2020-11-24 20:06:00 -0500

    Remove placeholder image from Homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -6,8 +6,7 @@ class Home extends Component {
     render() {
         return(
             <div>
-                <h1>Homepage</h1>
-                <img src="https://source.unsplash.com/Oh8Lp49y2Do/1000x700"></img>
+                Home Page
             </div>
         )
     }
