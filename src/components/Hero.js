ff2944364578b56bc89c50df9fa21f87cd79805f
leothebeagle <a.kosber@gmail.com>
2020-11-27 15:09:35 -0500

    Build a preliminary Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 const Hero = props => {
         return(
-            <div>
-                Hero Image Goes Here
+            <div className="hero">
+                <header>
+                    <h1>The Title of Your Hero Image</h1>
+                </header>
+                <footer>
+                    <h3>Your Call To Action Button</h3>
+                </footer>
             </div>
         )
 };
+
+// image for background:
+// unsplash id: MQzSZ5BkjAg
+// url: https://source.unsplash.com/{PHOTO ID}/{WIDTHxHEIGHT}
+// url: https://source.unsplash.com/MQzSZ5BkjAg
+
 
 
 export default Hero;
