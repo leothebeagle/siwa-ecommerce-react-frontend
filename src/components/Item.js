6d47b8fa95a9319dbfe9fb0483cb419eafd9c7fd
leothebeagle <a.kosber@gmail.com>
2020-12-01 01:17:17 -0500

    Include class names for sections

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,20 +2,23 @@ import React from 'react';
 
 const Item = props => {
     return(
-        <div className="item-container">
+        <div className="item-card">
         
-            <h4>{props.item.name}</h4> <br></br>
-            <img src={props.item.image}></img>
-            <p>{props.item.description}</p>
-            <p>{props.item.price}</p>
+            <div className="item-image">
+                <img src={props.item.image}></img>
+            </div>
+
+            <div className="item-info">
+                <h4>{props.item.name}</h4> <br></br>
+                <p>{props.item.description}</p>
+                <p>${props.item.price}</p>
+            </div>
+
             <button type="button" onClick={props.handleButtonClick}>{props.buttonText}</button>
-            {/* // dynamically populate the button text */}
-             {/* change onClick fn name in props to be generic */}
-             {/* onClick={props.handleButtonClick} */}
+
         </div>
     )
 }
-
 
 export default Item;
 
